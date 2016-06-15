67bf788cbd3de05a7bb430f066536c2550376595
董小敏 <1344091359@qq.com>
2016-06-15 13:22:09 +0800

    1
    1

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ a.controller('weixinCtrl',['$scope',function($scope){
   $scope.wxs = [
      {image:'./image/wx1.8.png',title:'订阅号',info:'山西农信：【今日头条】 临猗',data:'昨天',image2:'./image/wx1.6.png'},
      {image:'./image/wx1.3.png',title:'腾讯新闻',info:'白鹿原作者：陈忠实',data:'昨天',image2:'./image/wx1.6.png'},
-     {image:'./image/wx1.5.png',title:'微信团队',info:'微信：欢迎再次来到微信,data:'昨天',image2:'./image/wx1.7.png'},
+     {image:'./image/wx1.5.png',title:'微信团队',info:'微信：欢迎再次来到微信',data:'昨天',image2:'./image/wx1.7.png'},
      {image:'./image/弗尔斯特.png',title:'弗尔斯特 盛大星际俱乐部',info:'风影：壹购物',data:'刚刚',image2:'./image/wx1.7.png'},
   ]
 }])
